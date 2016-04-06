7ebbecf7bff2c39210d64b7eceb2924dce2d42e8
jgarciaamor <jgarciaamor@atanasoff01.danielcastelao.org>
2016-04-06 11:43:09 +0200

    boletin24_2

---
+++ src/boletin24_2/Boletin24_2.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package boletin24_2;
+
+/**
+ *
+ * @author jgarciaamor
+ */
+public class Boletin24_2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Frame24_2 boletin242 = new Frame24_2();
+        boletin242.setVisible(true);
+    }
+    
+}
